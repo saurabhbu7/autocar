522829abaf48c404b476eb26cd08306b772be909
saurabhbu7 <137925814+saurabhbu7@users.noreply.github.com>
2024-04-30 13:12:28 +0530

    Delete cypress_test_cases.js

--- cypress_test_cases.js
+++
@@ -1,9 +0,0 @@
-describe('Page HTML Content Test', () => {
-  it('Should display navbar links properly', () => {
-    // Test to check navbar links are displayed correctly
-  });
-  it('Should validate the card content', () => {
-    // Test to validate the card content
-  });
-  // Add more test cases as needed
-})
